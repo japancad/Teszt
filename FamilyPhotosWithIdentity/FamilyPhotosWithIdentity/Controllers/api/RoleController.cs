3bfd41d1e92e2d4f59949a0686f402ac2610cc18
Japan <ajkanetwork@gmail.com>
2018-08-13 22:39:09 +0200

    datatabels szürés lapozás

--- FamilyPhotosWithIdentity/FamilyPhotosWithIdentity/Controllers/api/RoleController.cs
+++ FamilyPhotosWithIdentity/FamilyPhotosWithIdentity/Controllers/api/RoleController.cs
@@ -39,8 +39,17 @@ namespace FamilyPhotosWithIdentity.Controllers.api
                 vm.Add(new RoleViewModel { UrlCode = role.UrlCode, Name = role.Name });
             }
 
+
+            //search müködés
+            var filteredVm = string.IsNullOrWhiteSpace(request?.Search.Value)
+                                        ? vm
+                                        : vm.Where(x => x.Name.Contains(request.Search.Value))
+                                            .ToList();
+
+            var vmPage = filteredVm.Skip(request.Start).Take(request.Length);
+
             //Elökésület DataTables válaszra
-            var response = DataTablesResponse.Create(request, vm.Count, vm.Count, vm);
+            var response = DataTablesResponse.Create(request, vm.Count, filteredVm.Count, vmPage);
             return new DataTablesJsonResult(response,true);
         }
     }
