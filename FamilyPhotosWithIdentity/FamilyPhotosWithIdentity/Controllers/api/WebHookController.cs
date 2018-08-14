14b8908d2b94274c12bc955d6b18f0e207c85cb1
Japan <ajkanetwork@gmail.com>
2018-08-15 00:08:22 +0200

    WebHook

---
+++ FamilyPhotosWithIdentity/FamilyPhotosWithIdentity/Controllers/api/WebHookController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FamilyPhotosWithIdentity.Models.Github;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FamilyPhotosWithIdentity.Controllers.api
+{
+
+    // http://requestbin.fullcontact.com/1isv82z1
+    [Produces("application/json")]
+    [Route("api/WebHook")]   //?api/github
+    
+    public class WebHookController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            var length = HttpContext.Request.ContentLength;
+            if (int.MaxValue<length)
+            {
+                throw new ArgumentOutOfRangeException($"Túl hosszú a kérés: {length} ");
+            }
+
+            var buffer = new byte[(int)HttpContext.Request.ContentLength];
+            var count = HttpContext.Request.Body.Read(buffer,0, (int)length);
+
+            if (count!=(int)length)
+            {
+                throw new Exception($"Hiba van : {count}");
+            }
+
+            var payload = Encoding.UTF8.GetString(buffer);
+
+
+            if (payload != null)
+            {
+                var request = JsonConvert.DeserializeObject<GithubRequest>(payload);
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ FamilyPhotosWithIdentity/FamilyPhotosWithIdentity/Models/Github/GithubRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyPhotosWithIdentity.Models.Github
+{
+    public class GithubRequest
+    {
+        public string zen { get; set; }
+        public string action { get; set; }
+        public int hook_id { get; set; }
+        public Hook hook { get; set; }
+        public Issue issue { get; set; }
+        public Repository repository { get; set; }
+        public Sender sender { get; set; }
+    }
+}
